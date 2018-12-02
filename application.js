19324ad3663305ac77ca6fa8f78996cf6dad60aa
Daniel Barbosa Maranhão <dbm2@cin.ufpe.br>
2018-12-02 11:20:55 -0300

    Implementing ButtonController

--- application.js
+++ application.js
@@ -1,22 +1,34 @@
 
-// var ButtonController = require('./controllers/sensors/button')
+// 
 // var CapacitiveController = require('./controllers/sensors//capacitive')
 // var USonicController = require('./controllers/sensors/usonic')
 
 var LedController = require('./controllers/sensors/led')
+var ButtonController = require('./controllers/sensors/button')
 
 const LEDS_PINS = [2, 3, 4]
+const BUTTONS_PINS = [17, 27, 22]
+const CAPACITIVE_PINS = [16]
+const USONIC_PINS = [{ echo: 21, trig: 20 }]
 
 class Application {
 
 	constructor() {
 		this.ledController = new LedController(LEDS_PINS)
+		this.buttonController = new ButtonController(BUTTONS_PINS)
 
-		this.ledController.setValue(LEDS_PINS[0], 1)
+		for (let buttonPin in BUTTONS_PINS)
+			this.buttonController.observeChanges(buttonPin, this.didReciveNewButtonState)
+
+		// this.ledController.setValue(LEDS_PINS[0], 1)
 	}
 
 	handleNewConnection(wsc) {
 
+	}
+
+	didReciveNewButtonState(pin, value) {
+		console.log(pin, value)
 	}
 }
 
---
+++ controllers/sensors/button.js
@@ -0,0 +1,36 @@
+var onoff = require('onoff');
+var Gpio = onoff.Gpio
+
+class ButtonController {
+
+	constructor(pins) {
+		this.buttons = []
+
+		for (let pin in pins) {
+			let button = {
+				io: new Gpio(pin, 'in', 'both', {debounceTimeout: 10}),
+				pin: pin
+			}
+
+			this.buttons.push(button)
+		}
+	}
+
+	observeChanges(pin, callback) {
+		let button = this.buttons.filter(button => button.pin == pin)[0]
+
+		if (button == null)
+			return
+
+		pushButton1.watch(function (error, value) {
+  			if (err) {
+    			console.log(error)
+    			return
+  			}
+
+  			callback(pin, value)
+		});
+	}
+}
+
+module.exports = ButtonController
